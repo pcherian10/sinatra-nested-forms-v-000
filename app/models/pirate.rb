914b2ee85a400921bbe99acf1cf24bdd2fd24f54
Paul <pcherian10@gmail.com>
2018-06-21 23:47:27 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+
+  attr_accessor :name, :weight,:height
+
+  PIRATES = []
+
+  def initialize (params)
+    @name, @height, @weight = params[:name], params[:height], params[:weight]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
